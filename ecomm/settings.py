4a0d7b11800edb217338c8222a2e9f2a6de22c1a
jkimunyi-dev@gmail.com <jkkimunyi@gmail.com>
2024-10-31 15:05:30 +0300

    Updated ecomm/settings.py to include blog userauths vendor store customer

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'blog',
+    'userauths',
+    'vendor',
+    'store',
+    'customer',
+
+    'anymail',
+    'captcha',
 ]
 
 MIDDLEWARE = [
